505ee701e428f08fb223dde7c274a105d6a700bc
igor1989 <ak@Computer.local>
2015-04-14 03:04:16 -0800

    Changed views and added events to results.html
    Added to sidebar:
       - Nutrition
       - Library
       - Online
       - Services
       - Filter option
    Home view:
       - Only displays items that are selected for homepage

--- arda_db/browser/views.py
+++ arda_db/browser/views.py
@@ -4,6 +4,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django import forms
 from models import Resource
 from models import REvent
+from models import RLibrary
+from models import ROnline
+from models import RService
 
 def paginate(request, resource_list):
     results_per_page = 5
@@ -21,7 +24,7 @@ def paginate(request, resource_list):
 
 
 def home(request):
-    resource_list = Resource.objects.order_by('r_id')
+    resource_list = Resource.objects.filter(homepage = True).order_by('r_id')
     context = {
         'resource_list': resource_list
     }
@@ -37,119 +40,130 @@ def events(request):
     return render_to_response('results.html', context)
 
 def result(request):
-    resource_list = []
+    resource_list = Resource.objects.all()
+    resource_type_list = []
     filter_url_addon = ""
     if 'male' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdemo__gender_m = True)))
+        resource_list = resource_list.filter(sdemo__gender_m = True)
         filter_url_addon += 'male=selected&'
     if 'female' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdemo__gender_f = True)))
+        resource_list = resource_list.filter(sdemo__gender_f = True)
         filter_url_addon += 'female=selected&'
     if 'age1-3' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdemo__age1to3 = True)))
+        resource_list = resource_list.filter(sdemo__age1to3 = True)
         filter_url_addon += 'age1-3=selected&'
     if 'age3-18' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdemo__age3to18 = True)))
-        filter_url_addon += 'age3-8=selected&'
+        resource_list = resource_list.filter(sdemo__age3to18 = True)
+        filter_url_addon += 'age3-18=selected&'
     if 'age18+' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdemo__age18plus = True)))
-        filter_url_addon += 'age18=selected&'
+        resource_list = resource_list.filter(sdemo__age18plus = True)
+        filter_url_addon += 'age18+=selected&'
     if 'Autism Spectrum Disorder' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdisorder__asd = True)))
+        resource_list = resource_list.filter(sdisorder__asd = True)
         filter_url_addon += 'Autism+Spectrum+Disorder=selected&'
     if 'Fetal Alcohol Syndrome' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdisorder__fas = True)))
+        resource_list = resource_list.filter(sdisorder__fas = True)
         filter_url_addon += 'Fetal+Alcohol+Syndrome=selected&'
     if 'Pervasive Developement Disorder' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdisorder__pdd = True)))
+        resource_list = resource_list.filter(sdisorder__pdd = True)
         filter_url_addon += 'Pervasive+Develeopment+Disorder=selected&'
     if 'Aspergers Syndrome' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdisorder__aspergers = True)))
+        resource_list = resource_list.filter(sdisorder__aspergers = True)
         filter_url_addon += 'Aspergers+Syndrome=selected&'
     if 'Cognative Development Disorder' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sdisorder__cdd = True)))
+        resource_list = resource_list.filter(sdisorder__cdd = True)
         filter_url_addon += 'Cognative+Development+Disorder=selected&'
     if 'sleep' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__sleep = True)))
+        resource_list = resource_list.filter(sbehaviour__sleep = True)
         filter_url_addon += 'sleep=selected&'
     if 'safety home' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__safety_home = True)))
+        resource_list = resource_list.filter(sbehaviour__safety_home = True)
         filter_url_addon += 'saftey+home=selected&'
     if 'safety public' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__safety_public = True)))
+        resource_list = resource_list.filter(sbehaviour__safety_public = True)
         filter_url_addon += 'safety+public=selected&'
     if 'safety travel' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__safety_travel = True)))
+        resource_list = resource_list.filter(sbehaviour__safety_travel = True)
         filter_url_addon += 'saftey+travel=selected&'
     if 'repetition' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__repetition = True)))
+        resource_list = resource_list.filter(sbehaviour__repetition = True)
         filter_url_addon += 'repetition=selected&'
     if 'aggression' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__aggression = True)))
+        resource_list = resource_list.filter(sbehaviour__aggression = True)
         filter_url_addon += 'aggression=selected&'
     if 'communication' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__communication = True)))
+        resource_list = resource_list.filter(sbehaviour__communication = True)
         filter_url_addon += 'communication=selected&'
     if 'nonverbal' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__nonverbal = True)))
+        resource_list = resource_list.filter(sbehaviour__nonverbal = True)
         filter_url_addon += 'nonverbal=selected&'
     if 'sensory' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__sensory = True)))
+        resource_list = resource_list.filter(sbehaviour__sensory = True)
         filter_url_addon += 'sensory=selected&'
     if 'meltdown' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__meltdown = True)))
+        resource_list = resource_list.filter(sbehaviour__meltdown = True)
         filter_url_addon += 'meltdown=selected&'
     if 'anxiety' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__anxiety = True)))
+        resource_list = resource_list.filter(sbehaviour__anxiety = True)
         filter_url_addon += 'anxiety=selected&'
     if 'change' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sbehaviour__change = True)))
+        resource_list = resource_list.filter(sbehaviour__change = True)
+        filter_url_addon += 'change=selected&'
+    if 'nutrition' in request.GET:
+        resource_list = resource_list.filter(sbehaviour__nutrition = True)
         filter_url_addon += 'change=selected&'
     if 'diagnostic' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sservices__diagnostic = True)))
+        resource_list = resource_list.filter(sservices__diagnostic = True)
         filter_url_addon += 'diagnostic=selected&'
     if 'resource' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sservices__resource = True)))
+        resource_list = resource_list.filter(sservices__resource = True)
         filter_url_addon += 'resource=selected&'
     if 'therapy' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sservices__therapy = True)))
+        resource_list = resource_list.filter(sservices__therapy = True)
         filter_url_addon += 'therapy=selected&'
     if 'educational' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sservices__educational = True)))
+        resource_list = resource_list.filter(sservices__educational = True)
         filter_url_addon += 'educational=selected&'
     if 'referral' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sservices__referral = True)))
+        resource_list = resource_list.filter(sservices__referral = True)
         filter_url_addon += 'referral=selected&'
     if 'legal' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sservices__legal = True)))
+        resource_list = resource_list.filter(sservices__legal = True)
         filter_url_addon += 'legal=selected&'
     if 'city' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sservices__city = True)))
+        resource_list = resource_list.filter(sservices__city = True)
         filter_url_addon += 'city=selected&'
     if 'parents' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sadditional__parents = True)))
+        resource_list = resource_list.filter(sadditional__parents = True)
         filter_url_addon += 'parents=selected&'
     if 'relationships' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sadditional__relationships = True)))
+        resource_list = resource_list.filter(sadditional__relationships = True)
         filter_url_addon += 'relationships=selected&'
     if 'teachers' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sadditional__teachers = True)))
+        resource_list = resource_list.filter(sadditional__teachers = True)
         filter_url_addon += 'teachers=selected&'
     if 'siblings' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sadditional__sibilings = True)))
+        resource_list = resource_list.filter(sadditional__sibilings = True)
         filter_url_addon += 'siblings=selected&'
     if 'teens' in request.GET:
-        resource_list.extend(list(Resource.objects.filter(sadditional__teens = True)))
+        resource_list = resource_list.filter(sadditional__teens = True)
         filter_url_addon += 'teens=selected&'
+    
+    if 'library' in request.GET:
+        resource_type_list.extend(list(resource_list.filter(r_id__in = RLibrary.objects.all())))
+        filter_url_addon += 'library=selected&'
+    if 'online' in request.GET:
+        resource_type_list.extend(list(resource_list.filter(r_id__in = ROnline.objects.all())))
+        filter_url_addon += 'online=selected&'
+    if 'service' in request.GET:
+        resource_type_list.extend(list(resource_list.filter(r_id__in = RService.objects.all())))
+        filter_url_addon += 'service=selected&'
     
     # Delete the last character & 
     filter_url_addon = filter_url_addon[:-1]
-    
-    # Remove duplicates in the list
-    resource_list = list(set(resource_list))    
-        
+            
     context = {
-        "content": paginate(request, resource_list),
+        "content": paginate(request, resource_type_list),
         "filter_url_addon": filter_url_addon,
         "page_url": "/result/"
     }
