ae51f4b127f9c2fbb4769b200bfebda7d3134a3f
rawser spicer <rwspicer@alaska.edu>
2015-04-23 12:38:19 -0800

    set up folder and setings for serving the statuc files

--- arda_db/arda_db/settings.py
+++ arda_db/arda_db/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 EMAIL_HOST = 'localhost'
