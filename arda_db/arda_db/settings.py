b0a15121a8792001a266ebe4d8275efe25fb5d99
rawser spicer <rwspicer@alaska.edu>
2015-04-23 12:12:55 -0800

    updated database name

--- arda_db/arda_db/settings.py
+++ arda_db/arda_db/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'arda_db.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'arda_db.sqlite3'),
         #'USER': 'arda-admin',
 		#'PASSWORD': 'ICannotRememberTheLastTimeI8',
     }
